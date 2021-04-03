a11ca10a136fa598c5d767fff4bc569dc5efce46
Coast8 <smythy.costa@gmail.com>
2021-04-03 06:06:25 -0300

    DUAL

---
+++ Chapter_001-Basico1/from_dual.sql
@@ -0,0 +1,9 @@
+
+
+
+-- The Dual table is widely used by developers, 
+-- DBAs and Oracle itself to perform operations with select where it is not necessary to extract data in tables.
+
+SELECT SYSDATE FROM dual;
+
+SELECT SEQUENCE_VALUE.CURRVAL FROM DUAL;
